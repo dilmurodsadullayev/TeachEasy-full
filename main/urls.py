671d840c04091e9b3f3b3835ef48e393ac701aa6
Dilmurod <142794823+d1ilmurod@users.noreply.github.com>
2024-10-30 10:10:54 +0500

    first commit

---
+++ main/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.i18n import urlpatterns
+from django.urls import path
+from .views import (
+    index_view,
+    about_view,
+    contact_view,
+    courses_view,
+    teachers_view,
+    gallery_view,
+    single_view,
+    blogs_view
+)
+
+urlpatterns = [
+    path('',index_view,name='index'),
+    path('about',about_view,name='about'),
+    path('contact',contact_view,name='contact'),
+    path('courses',courses_view,name='courses'),
+    path('teachers',teachers_view,name='teachers'),
+    path('gallery',gallery_view,name='gallery'),
+    path('single',single_view,name='single'),
+    path('blogs',blogs_view,name='blogs'),
+
+]
---
+++ main/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index_view(request):
+    return render(request, 'main/index.html')
+
+
+
+def about_view(request):
+    return render(request, 'main/about.html')
+
+
+def contact_view(request):
+    return render(request, 'main/contact.html')
+
+def courses_view(request):
+    return render(request, 'main/courses.html')
+
+def teachers_view(request):
+    return render(request, 'main/teachers.html')
+
+
+def gallery_view(request):
+    return render(request, 'main/gallery.html')
+
+def single_view(request):
+    return render(request, 'main/single.html')
+
+def blogs_view(request):
+    return render(request, 'main/blog.html')
